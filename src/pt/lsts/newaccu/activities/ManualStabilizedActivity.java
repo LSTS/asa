3ddb4914eef8017dde866eebc600b96227b34886
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2014-10-30 16:23:16 +0000

    Added Fullscreen blank activity for Manual/Stabilized modes

---
+++ src/pt/lsts/newaccu/activities/ManualStabilizedActivity.java
@@ -0,0 +1,23 @@
+package pt.lsts.newaccu.activities;
+
+import pt.lsts.newaccu.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ManualStabilizedActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_manual_stabilized);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+	}
+
+
+}
