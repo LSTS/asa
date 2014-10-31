9e4ec8821977c689b15f03f2c8c5a10740821874
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2014-10-31 16:09:09 +0000

    Added Fullscreen blank Activity for PFD | Added Google Maps permissions and API key to Manifest

---
+++ src/pt/lsts/newaccu/PfdActivity.java
@@ -0,0 +1,19 @@
+package pt.lsts.newaccu;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PfdActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pfd);
+	}
+
+}
