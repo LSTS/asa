e0cd036c4b0e3b841f4c02ee8690b1e90c4f59db
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2014-11-20 17:02:55 +0000

    Added ImcSystem and DataFragment from Triton

---
+++ src/pt/lsts/asa/comms/ImcSystem.java
@@ -0,0 +1,100 @@
+package pt.lsts.asa.comms;
+
+import java.text.DecimalFormat;
+
+import pt.lsts.imc.EstimatedState;
+import pt.lsts.util.WGS84Utilities;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+public class ImcSystem {
+	private static final String TAG = "ImcSystem";
+	private String imcName;
+	private final int imcId;
+	private float height;
+	private float speed;
+	private LatLng location;
+	private float rotation;
+
+	public ImcSystem (EstimatedState msg){
+		imcName = msg.getSourceName();
+		imcId = msg.getSrc();
+		update(msg);
+	}
+	
+	// For creating dummy data
+	public ImcSystem(int imcId, String imcName, float height, float speed,
+			LatLng location, float rotation) {
+		super();
+		this.imcName = imcName;
+		this.imcId = imcId;
+		this.height = height;
+		this.speed = speed;
+		this.location = location;
+		this.rotation = rotation;
+	}
+
+	public void update(EstimatedState state){
+		// Log.i(TAG, "IMC " + imcName + ": "
+		// + " [" + state.getLat() + "," + state.getLon() + "], ["
+		// + state.getX() + " " + state.getY() + "]");
+		// Location
+		double[] wgs84displace = WGS84Utilities.WGS84displace(
+				Math.toDegrees(state.getLat()),
+				Math.toDegrees(state.getLon()), state.getHeight(),
+				state.getX(), state.getY(), state.getZ());
+		location = new LatLng((wgs84displace[0]), wgs84displace[1]);
+		// Height
+		// if (state.getZ() < 0) {
+		// this.height = (float) state.getZ();
+		// }
+		// else{
+		this.height = (float) (state.getHeight() - state.getZ()); // wgs84displace[2];
+		// }
+		// Speed
+		this.speed = (float)state.getU();
+		// Rotation
+		rotation = (float) Math.toDegrees(state.getPsi());
+		// Log.i(TAG, imcName + " updated to " + height + "m, " + speed
+		// + "m/s, [" + location.latitude + "," + location.longitude
+		// + "] " + rotation + "º");
+		imcName = state.getSourceName();
+	}
+	
+	public String getName(){
+		return imcName;
+	}
+
+	public void updateMarker(Marker marker) {
+		marker.setTitle(imcName);
+		marker.setPosition(location);
+		marker.setRotation(rotation);
+		DecimalFormat df = new DecimalFormat("#.##");
+		String snippet = "Height " + df.format(height) + "; Speed: "
+				+ df.format(speed) + "; Heading:" + rotation;
+		marker.setSnippet(snippet);
+		Log.i(TAG, "Updating " + imcName + " to " + location.toString());
+	}
+
+	public float getHeight() {
+		return height;
+	}
+	
+
+	public String getImcName() {
+		return imcName;
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * @return the IMC Id
+	 */
+	public int getImcId() {
+		return imcId;
+	}
+}
---
+++ src/pt/lsts/asa/fragments/DataFragment.java
@@ -0,0 +1,95 @@
+package pt.lsts.asa.fragments;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import pt.lsts.asa.comms.ImcSystem;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.model.LatLng;
+
+@SuppressLint("UseSparseArrays")
+public class DataFragment extends Fragment{
+	public static final String TAG = "DataFragment";
+	private HashMap<Integer, ImcSystem> knownSystems;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		Log.i(TAG, "DataFragment onCreateView.");
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		Log.i(TAG, "DataFragment onAttach.");
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Log.i(TAG, "DataFragment onActivityCreated.");
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		knownSystems = new HashMap<Integer, ImcSystem>();
+		addDummySystems();
+		setRetainInstance(true);
+		Log.i(TAG, "DataFragment onCreate.");
+	}
+	
+	public void addSystem(ImcSystem sys){
+		knownSystems.put(sys.getImcId(), sys);
+	}
+
+	public ImcSystem getSystem(int imcId) {
+		return knownSystems.get(imcId);
+	}
+
+	public Set<Integer> getimcIdAllSytems() {
+		return knownSystems.keySet();
+	}
+
+	private void addDummySystems(){
+		// Test X8-00
+		String title = "Test X8-00";
+		LatLng coord = new LatLng(0, 0);
+		int id = 99;
+		int rotation = 90;
+		float heightIn = 100f;
+		float speedIn = 18.2f;
+		ImcSystem sys = new ImcSystem(id, title, heightIn, speedIn, coord,
+				rotation);
+		knownSystems.put(id, sys);
+		// Test X8-02
+		title = "Test X8-02";
+		id = 98;
+		coord = new LatLng(0, 20.5);
+		rotation = 45;
+		heightIn = 180f;
+		speedIn = 15.3f;
+		sys = new ImcSystem(id, title, heightIn, speedIn, coord, rotation);
+		knownSystems.put(id, sys);
+	}
+
+	public ImcSystem getSystem(String title) {
+		Set<Integer> allImcIds = knownSystems.keySet();
+		for (Integer imcId : allImcIds) {
+			ImcSystem imcSystem = knownSystems.get(imcId);
+			if (imcSystem.getImcName().equals(title))
+				return imcSystem;
+		}
+		return null;
+	}
+
+}
