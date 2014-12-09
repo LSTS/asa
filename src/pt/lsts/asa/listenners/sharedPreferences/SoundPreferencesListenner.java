ac3ba6c6dc2bf1bee70f9e7b2352681c77792be6
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2014-12-09 11:13:00 +0000

    Changed settings to be all lowercase | Added listeners for SoundPreferences

--- src/pt/lsts/asa/listenners/sharedPreferences/SoundPreferencesListenner.java
+++ src/pt/lsts/asa/listenners/sharedPreferences/SoundPreferencesListenner.java
@@ -1,21 +1,62 @@
 package pt.lsts.asa.listenners.sharedPreferences;
 
+import java.util.Locale;
+
 import pt.lsts.asa.ASA;
 import pt.lsts.asa.feedback.CallOut;
+import pt.lsts.asa.settings.Settings;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.util.Log;
 
 public class SoundPreferencesListenner implements OnSharedPreferenceChangeListener{
-	CallOut callout = ASA.getInstance().getCallOut();
+	private final String TAG = "SoundPreferencesListenner";
+	private CallOut callOut = ASA.getInstance().getCallOut();
+	private Context context;
+	
+	public SoundPreferencesListenner(Context context){
+		this.context=context;
+	}
 
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
-		String keyLowerCase = key.toLowerCase();
+		
+		String keyLowerCase = key.toLowerCase(Locale.getDefault());
+		int integer;
+		boolean bool;
+		String string;
 		switch(keyLowerCase){
-				
+			case "audio_global_audio":
+				bool = Settings.getBoolean(key, true);
+				callOut.setGlobalMuteBool(bool);
+				break;
+			case "audio_alt_audio":
+				bool = Settings.getBoolean(key, true);
+				callOut.setAltMuteBool(bool);
+				break;
+			case "audio_ias_audio":
+				bool = Settings.getBoolean(key, true);
+				callOut.setIasMuteBool(bool);
+				break;
+			case "audio_alt_interval_in_seconds":
+				integer = Settings.getInt(key, 10)*1000;
+				callOut.setAltInterval(integer);
+				break;
+			case "audio_ias_interval_in_seconds":
+				integer = Settings.getInt(key, 10)*1000;
+				callOut.setIasInterval(integer);
+				break;
+			case "audio_timeout_interval_in_seconds":
+				integer = Settings.getInt(key, 10)*1000;
+				callOut.setTimeoutInterval(integer);
+				break;
+			
 			default:
+				Log.e(TAG,"Setting changed unrecognized");
 				break;
 		}
 	}
+	
 }
