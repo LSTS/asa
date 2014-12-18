01673b33a71e81254279e5ee684e188d4603eeeb
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2014-12-18 14:34:29 +0000

    Updated cam_ip to cam_ip_port | Added aircam support | Not functional: Android not compatible with RTSP over TCP.

--- src/pt/lsts/asa/util/StringUtils.java
+++ src/pt/lsts/asa/util/StringUtils.java
@@ -17,22 +17,54 @@ public class StringUtils {
 		switch (cam){
 			case "axis":
 				return getAxisUrl(videoView);
+			case "aircam":
+				return getAirCamUrl(videoView);
 			default:
 				return "ERROR";
 		}
 
 	}
 
-	public static String getAxisUrl(VideoView videoView){
+	public static String getAirCamUrl(VideoView videoView){
+		//rtsp://<CAM_IP>:554/live/ch00_0 - Full resolution - 1280 (w) x 720 (h)
 		String protocol = Settings.getString("cam_protocol", "rtsp");
-		String ip= Settings.getString("cam_ip", "10.0.20.199");
+		String ip_port= Settings.getString("cam_ip_port", "10.0.20.102:554");
+		String location = "live";
+
+		String resolution = Settings.getString("cam_resolution", "0x0");
+		resolution = resolution.toLowerCase(Locale.UK);
+		switch (resolution){
+			case "160x96":
+				resolution="ch03_0";
+				break;
+			case "320x176":
+				resolution="ch02_0";
+				break;
+			case "640x368":
+				resolution="ch01_0";
+				break;
+			case "1280x720":
+			default:
+				resolution="ch00_0";
+		}
+
+		//resolution = validateResolution(resolution, videoView);
+
+		String completeUrl=protocol+"://"+ip_port+"/"+location+"/"+resolution;
+		return completeUrl;
+	}
+
+	public static String getAxisUrl(VideoView videoView){
+		//rtsp://IPADDRESS/axis-media/media.amp?videocodec=h264&resolution=640x480
+		String protocol = Settings.getString("cam_protocol", "rtsp");
+		String ip_port= Settings.getString("cam_ip", "10.0.20.199");
 		String location = "axis-media/media.amp";
 		String codec = Settings.getString("cam_codec","h264");
 		String resolution = Settings.getString("cam_resolution", "0x0");
 
 		resolution = validateResolution(resolution, videoView);
 
-		String completeUrl=protocol+"://"+ip+"/"+location+"?videocodec="+codec+"&resolution="+resolution;
+		String completeUrl=protocol+"://"+ip_port+"/"+location+"?videocodec="+codec+"&resolution="+resolution;
 		return completeUrl;
 	}
 
