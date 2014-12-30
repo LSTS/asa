303b52c00d430c1ad416ea7a9539ee3c2c311294
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2014-12-30 14:34:27 +0000

    Added methods to place markers and get Location | Place a marker on last known location

--- src/pt/lsts/asa/fragments/GmapFragment.java
+++ src/pt/lsts/asa/fragments/GmapFragment.java
@@ -1,42 +1,42 @@
 package pt.lsts.asa.fragments;
 
 import pt.lsts.asa.R;
-import pt.lsts.asa.R.id;
-import pt.lsts.asa.R.layout;
 
 import com.google.android.gms.maps.GoogleMap;
 //import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass. Activities that contain this fragment
- * must implement the {@link GmapFragment.OnFragmentInteractionListener}
- * interface to handle interaction events. Use the
- * {@link GmapFragment#newInstance} factory method to create an instance of this
- * fragment.
- *
- */
-public class GmapFragment extends Fragment {
+public class GmapFragment extends Fragment implements OnMapReadyCallback {
 
-	Context context;
+    FragmentActivity fragmentActivity=null;
 	GoogleMap googleMap = null;
+    MapFragment mapFragment = null;
+
 
 	public GmapFragment() {
 		// Required empty public constructor
 	}
 
-	public GmapFragment(Context context) {
-		this.context = context;
+	public GmapFragment(FragmentActivity fragmentActivity) {
+		this.fragmentActivity = fragmentActivity;
 	}
 
 	@Override
@@ -62,9 +62,48 @@ public class GmapFragment extends Fragment {
 		super.onDetach();
 	}
 
-	public void startGmap() {
-		googleMap = ((SupportMapFragment) getChildFragmentManager()
-				.findFragmentById(R.id.map)).getMap();
-	}
+    public void initMapFragment() {
+        mapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction = fragmentActivity.getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container_auto, mapFragment);
+        fragmentTransaction.commit();
+
+        MapFragment mapFragment = (MapFragment) fragmentActivity.getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        //map.setMyLocationEnabled(true);//set a blue dot on position
+        this.googleMap=map;
+        addMarkerToMyPos();
+    }
+
+    public void addMarkerToMyPos(){
+        addMarkerToPos("myLocation",getLastKnowLatLng());
+    }
+
+    public void addMarkerToPos(String title, LatLng latLng){
+        this.googleMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title(title));
+    }
+
+    public LatLng getLastKnowLatLng(){
+        double lat = 0;
+        double lon = 0;
+
+        LocationManager lm;
+        lm = (LocationManager) fragmentActivity.getSystemService(Context.LOCATION_SERVICE);
+        Location lastKnownLoc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (lastKnownLoc != null) {
+            lat = (int) lastKnownLoc.getLatitude() ;
+            lon = (int) lastKnownLoc.getLongitude();
+        }
+        LatLng latlng = new LatLng(lat, lon);
+        return latlng;
+    }
 
 }
