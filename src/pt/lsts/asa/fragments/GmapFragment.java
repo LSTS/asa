680f937f48af74a4ba395bdb29dd3bdb5d1b3d80
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2015-01-27 13:25:15 +0000

    Added: GmapIMCSubscriber, methods to add marker for sys, created marker for activeSys with updates of position without orientation

--- src/pt/lsts/asa/fragments/GmapFragment.java
+++ src/pt/lsts/asa/fragments/GmapFragment.java
@@ -1,7 +1,10 @@
 package pt.lsts.asa.fragments;
 
+import pt.lsts.asa.ASA;
 import pt.lsts.asa.R;
 import pt.lsts.asa.listenners.MyLocationListener;
+import pt.lsts.asa.subscribers.GmapIMCSubscriber;
+import pt.lsts.asa.sys.Sys;
 
 import android.app.Activity;
 import android.location.Location;
@@ -20,28 +23,35 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class GmapFragment extends Fragment implements OnMapReadyCallback {
 
+    public static final String TAG = "GmapFragment";
     private FragmentActivity fragmentActivity=null;
 
     private GoogleMap googleMap = null;
     private MapFragment mapFragment = null;
+
     private LatLng myLatLng = new LatLng(0,0);
     private MyLocationListener myLocationListener = null;
     private Boolean initZoom = false;
 
+    private ArrayList<Marker> markersArrayList = new ArrayList<Marker>();//markers of vehicles positions
 
-	public GmapFragment() {
-		// Required empty public constructor
-	}
 
-	public GmapFragment(FragmentActivity fragmentActivity) {
-		this.fragmentActivity = fragmentActivity;
+    public GmapFragment() {
+        // Required empty public constructor
+    }
+
+    public GmapFragment(FragmentActivity fragmentActivity) {
+        this.fragmentActivity = fragmentActivity;
         myLocationListener = new MyLocationListener(this);
         myLocationListener.initLocationListener();
-	}
+    }
 
     public FragmentActivity getFragmentActivity() {
         return fragmentActivity;
@@ -56,48 +66,77 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		View v = inflater.inflate(R.layout.fragment_gmaps, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_gmaps, container, false);
         mapFragment = (MapFragment) fragmentActivity.getFragmentManager().findFragmentById(R.id.googleMap);
         mapFragment.getMapAsync(this);
-		return v;
-	}
+        return v;
+    }
 
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
-	}
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
 
-	@Override
-	public void onDetach() {
-		super.onDetach();
-	}
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public void initIMCSubscriber(){
+        GmapIMCSubscriber gmapIMCSubscriber = new GmapIMCSubscriber(this);
+        ASA.getInstance().addSubscriber(gmapIMCSubscriber);
+    }
 
     @Override
     public void onMapReady(GoogleMap map) {
         Log.i("onMapReady", "onMapReady");
         googleMap=map;
         googleMap.setMyLocationEnabled(true);//set a blue dot on position
+        initIMCSubscriber();
     }
 
-    public void addMarkerToPos(String title, LatLng latLng){
-        this.googleMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .title(title));
+    public Marker addMarkerToPos(final Sys sys, final String title, final LatLng latLng){//standard googles red marker
+        if (googleMap!=null) {
+
+            fragmentActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Marker marker = googleMap.addMarker(new MarkerOptions()
+                                    .position(latLng)
+                                    .title(title)
+                    );
+                    sys.setMaker(marker);
+                }
+            });
+        }
+        return sys.getMaker();
     }
 
-    public void addMarkerToPos(String title, LatLng latLng, int iconRid){//R.drawable.icon
-        this.googleMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .title(title)
-                .icon(BitmapDescriptorFactory.fromResource(iconRid)));
+    public Marker addMarkerToPos(final Sys sys, final String title, final LatLng latLng, final int iconRid){//R.drawable.icon
+        if (googleMap!=null) {
+
+            fragmentActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Marker marker = googleMap.addMarker(new MarkerOptions()
+                                    .position(latLng)
+                                    .title(title)
+                                    .icon(BitmapDescriptorFactory.fromResource(iconRid))
+
+                    );
+                    sys.setMaker(marker);
+                }
+            });
+        }
+        return sys.getMaker();
     }
 
     public void updateMyLocation(Location location){
@@ -109,10 +148,36 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
             lon = location.getLongitude();
         }
         if (initZoom==false && googleMap!=null){//center camera initialy on my position
-            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(lat,lon),16.0f,1.0f,1.0f)));
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(lat,lon),6.0f,1.0f,1.0f)));
             initZoom=true;
         }
         this.myLatLng = new LatLng(lat, lon);
+    }
+
+    public void setActiveSysLatLng(final LatLng vehicleLatLng) {
+        ASA.getInstance().getActiveSys().setLatLng(vehicleLatLng);
+        final String vehicleName = ASA.getInstance().getActiveSys().getName().toString();
+        if (ASA.getInstance().getActiveSys().getMaker()==null)
+            addMarkerToPos(ASA.getInstance().getActiveSys(), vehicleName, vehicleLatLng, R.drawable.ic_orange_arrow);
+        else {
+            fragmentActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ASA.getInstance().getActiveSys().getMaker().setPosition(vehicleLatLng);
+                    Log.i(TAG, "setPosition("+vehicleLatLng.toString()+")");
+                }
+            });
+        }
+    }
+
+    public void removeMarker(String title){
+        for (Marker marker : markersArrayList){
+            if (marker.getTitle().equalsIgnoreCase(title)) {
+                markersArrayList.remove(marker);
+                marker.remove();
+                return;
+            }
+        }
     }
 
 }
---
+++ src/pt/lsts/asa/subscribers/GmapIMCSubscriber.java
@@ -0,0 +1,70 @@
+package pt.lsts.asa.subscribers;
+
+import pt.lsts.asa.comms.IMCSubscriber;
+import pt.lsts.asa.fragments.GmapFragment;
+import pt.lsts.asa.util.IMCUtils;
+import pt.lsts.imc.EstimatedState;
+import pt.lsts.imc.IMCMessage;
+
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+
+
+
+/**
+ * Created by jloureiro on 1/22/15.
+ */
+public class GmapIMCSubscriber implements IMCSubscriber {
+
+    private final String TAG = "GmapIMCSubscriber";
+    private GmapFragment gmapfragment = null;
+    private Thread thread;
+    private float r_earth = 6378 * 1000; //meters
+
+    public GmapIMCSubscriber(GmapFragment gmapFragment){
+        this.gmapfragment=gmapFragment;
+    }
+
+    @Override
+    public void onReceive(final IMCMessage msg) {
+        if (thread!=null)//if there is a previous message, finish going through that one
+            while (thread.isAlive());
+
+        thread = new Thread() {
+            @Override
+            public void run() {
+                Log.v(TAG, "Received Message");
+                if (IMCUtils.isMsgFromActive(msg)){
+                    Log.v(TAG,"Message from active:"+msg.getAbbrev());
+                    final int ID_MSG = msg.getMgid();
+                    if (ID_MSG == EstimatedState.ID_STATIC){
+                        Double latRad = msg.getDouble("lat");
+                        Double lonRad = msg.getDouble("lon");
+                        Double latDeg = (Double) Math.toDegrees(latRad);
+                        Double lonDeg = (Double) Math.toDegrees(lonRad);
+
+                        float offsetX = msg.getFloat("x");//offset north
+                        float offsetY = msg.getFloat("y");//offset east
+
+                        LatLng latLng = translateCoordinates(new LatLng(latDeg,lonDeg), offsetX, offsetY);
+
+                        Log.i(TAG,"latLng="+latLng.toString());
+                        gmapfragment.setActiveSysLatLng(latLng);
+                    }
+                }
+
+            }
+        };
+        thread.start();
+    }
+
+    public LatLng translateCoordinates(final LatLng origpoint, final float offsetX, final float offsetY) {
+        final double earthRadius = 6371000;
+
+        final double newLat = origpoint.latitude + (offsetX / earthRadius) * 180 / Math.PI;
+        final double newLon = origpoint.longitude + (offsetY / (earthRadius * Math.cos(newLat * 180 / Math.PI))) * 180 / Math.PI;
+
+        return new LatLng(newLat, newLon);
+    }
+}
