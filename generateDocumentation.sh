4b4d83972661038bfc7a626683f8b569ca9301f9
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2015-01-28 10:39:47 +0000

    Added doxygen generation config file and script

---
+++ generateDocumentation.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+doxygen asa.doxyfile
+
+echo "------------------------------------------------------------------------"
+echo ""
+echo "main html file in doxygenOutput/html/index.html"
+echo ""
+
