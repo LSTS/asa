13620b387f35a720da9979b2653dfcbf741df1b3
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2015-02-10 16:42:57 +0000

    Added translateCoordinates to GmapsUtil

--- src/pt/lsts/asa/util/GmapsUtil.java
+++ src/pt/lsts/asa/util/GmapsUtil.java
@@ -1,6 +1,14 @@
 package pt.lsts.asa.util;
 
+import android.util.Log;
+
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+import pt.lsts.asa.ASA;
+import pt.lsts.asa.sys.Sys;
 
 /**
  * Created by jloureiro on 1/27/15.
@@ -27,6 +35,37 @@ public class GmapsUtil {
         float result = psi-90;
         result = result + googleMap.getCameraPosition().bearing;
         return result;
+    }
+
+    public static LatLng translateCoordinates(final LatLng origpoint, final float offsetX, final float offsetY) {
+        /*
+        final double earthRadius = 6371000;
+
+        final double newLat = origpoint.latitude + (offsetX / earthRadius) * 180 / Math.PI;
+        final double newLon = origpoint.longitude + (offsetY / (earthRadius * Math.cos(newLat * 180 / Math.PI))) * 180 / Math.PI;
+
+        return new LatLng(newLat, newLon);
+        */
+        //Position, decimal degrees
+        double lat = origpoint.latitude;
+        double lon = origpoint.longitude;
+
+        //Earth’s radius, sphere
+        double R=6378137;
+
+        //offsets in meters
+        double dn = offsetX;
+        double de = offsetY;
+
+        //Coordinate offsets in radians
+        double dLat = dn/R;
+        double dLon = de/(R*Math.cos(Math.PI * lat / 180));
+
+        //OffsetPosition, decimal degrees
+        double latO = lat + dLat * 180/Math.PI;
+        double lonO = lon + dLon * 180/Math.PI;
+
+        return new LatLng(latO, lonO);
     }
 
 }
