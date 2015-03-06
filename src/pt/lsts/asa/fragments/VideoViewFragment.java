00ec81cb291873914f69d4ffa834dd70ed73b9c1
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2015-03-06 10:13:07 +0000

    Cleanup

--- src/pt/lsts/asa/fragments/VideoViewFragment.java
+++ src/pt/lsts/asa/fragments/VideoViewFragment.java
@@ -1,21 +1,14 @@
 package pt.lsts.asa.fragments;
 
+
 import pt.lsts.asa.R;
 import pt.lsts.asa.util.AndroidUtil;
 import pt.lsts.asa.util.StringUtils;
-import pt.lsts.asa.util.mjpeg.MjpegInputStream;
 import pt.lsts.asa.util.mjpeg.MjpegService;
 import pt.lsts.asa.util.mjpeg.MjpegView;
 
-import java.io.IOException;
-import java.net.URI;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -26,10 +19,6 @@ import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 public class VideoViewFragment extends Fragment {
 
@@ -107,16 +96,6 @@ public class VideoViewFragment extends Fragment {
 
     public void startVideo(){
         initMjpegView();
-/*
-        //String camUrl = "http://trackfield.webcam.oregonstate.edu/axis-cgi/mjpg/video.cgi?resolution=800x600&amp%3bdummy=1333689998337";//test public ip cam
-        String camUrl = StringUtils.getCamUrl();//"http://10.0.20.112/axis-cgi/mjpg/video.cgi?date=0&clock=0&camera=1&resolution=640x480";
-        Log.i(TAG,"URL: "+camUrl);
-        if (task!=null){
-            task.cancel(true);
-            task = null;
-        }
-        task = new getMjpegInputStreamAsyncTask().execute(camUrl);
-*/
         startMjpegService();
     }
 
@@ -132,39 +111,5 @@ public class VideoViewFragment extends Fragment {
         }
     }
 
-    /*
-    public class getMjpegInputStreamAsyncTask extends AsyncTask<String, Void, MjpegInputStream> {
-        protected MjpegInputStream doInBackground(String... url) {
-            HttpResponse res = null;
-            DefaultHttpClient httpclient = new DefaultHttpClient();
-            Log.d(TAG, "1. Sending http request");
-            try {
-                res = httpclient.execute(new HttpGet(URI.create(url[0])));
-                Log.d(TAG, "2. Request finished, status = " + res.getStatusLine().getStatusCode());
-                if(res.getStatusLine().getStatusCode()==401){
-                    //You must turn off camera User Access Control before this will work
-                    return null;
-                }
-                return new MjpegInputStream(res.getEntity().getContent());
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-                Log.d(TAG, "Request failed-ClientProtocolException", e);
-                //Error connecting to camera
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.d(TAG, "Request failed-IOException", e);
-                //Error connecting to camera
-            }
-
-            return null;
-        }
-
-        protected void onPostExecute(MjpegInputStream result) {
-            mjpegView.setSource(result);
-            mjpegView.setDisplayMode(MjpegView.SIZE_FULLSCREEN);
-            mjpegView.showFps(true);
-        }
-    }
-    */
 
 }
