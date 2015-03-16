f608b84264efbf6fd79f9388533b4707c044d0ce
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2015-03-16 17:13:42 +0000

    Added AltitudeFromPlanTextView and made Text from textView equal the alt

--- app/src/main/java/pt/lsts/asa/fragments/AltitudeBarFragment.java
+++ app/src/main/java/pt/lsts/asa/fragments/AltitudeBarFragment.java
@@ -4,12 +4,12 @@ package pt.lsts.asa.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import pt.lsts.asa.R;
 
@@ -22,6 +22,8 @@ public class AltitudeBarFragment extends Fragment {
     public static final String TAG = "AltitudeBarFragment";
     private FragmentActivity fragmentActivity=null;
     private ImageView altitudeBarDroneIconimageView = null;
+    private TextView altitudeFromVehicleTextView=null;
+    private TextView altitudeFromPlanTextView=null;
 
     public AltitudeBarFragment() {
         // Required empty public constructor
@@ -35,23 +37,42 @@ public class AltitudeBarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_altitude_bar, container, false);
+
         altitudeBarDroneIconimageView = (ImageView) v.findViewById(R.id.altitudeBarDroneIconimageView);
+        altitudeFromVehicleTextView = (TextView) v.findViewById(R.id.altitudeFromVehicleTextView);
+        altitudeFromPlanTextView = (TextView) v.findViewById(R.id.altitudeFromPlanTextView);
+
         return v;
     }
 
     @Override
     public void onResume(){
         super.onResume();
-        setAlt(700);//testing
+        setVehicleAlt(700);//testing
+        setPlanAlt(250);//testing
     }
 
     /**
      *
      * @param alt may be bettween 0 and 1000, available range.
      */
-    public void setAlt(int alt){
+    public void setVehicleAlt(int alt){
         FrameLayout.LayoutParams frameLayoutParams = (FrameLayout.LayoutParams) altitudeBarDroneIconimageView.getLayoutParams();
         frameLayoutParams.setMargins(frameLayoutParams.leftMargin,frameLayoutParams.topMargin,frameLayoutParams.rightMargin,(80-(35)) + alt);//80 bottom margin; 35 is the adjustment value for icon positioning
+
+        altitudeFromVehicleTextView.setText(""+alt);
+        frameLayoutParams = (FrameLayout.LayoutParams) altitudeFromVehicleTextView.getLayoutParams();
+        frameLayoutParams.setMargins(frameLayoutParams.leftMargin,frameLayoutParams.topMargin,frameLayoutParams.rightMargin,(80-(25)) + alt);//80 bottom margin; 35 is the adjustment value for icon positioning
+    }
+
+    /**
+     *
+     * @param alt may be bettween 0 and 1000, available range.
+     */
+    public void setPlanAlt(int alt){
+        altitudeFromPlanTextView.setText(""+alt);
+        FrameLayout.LayoutParams frameLayoutParams = (FrameLayout.LayoutParams) altitudeFromPlanTextView.getLayoutParams();
+        frameLayoutParams.setMargins(frameLayoutParams.leftMargin,frameLayoutParams.topMargin,frameLayoutParams.rightMargin,(80-(25)) + alt);//80 bottom margin; 35 is the adjustment value for icon positioning
     }
 
 
