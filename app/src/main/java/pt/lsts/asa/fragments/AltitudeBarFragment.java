77e6053406b256f4c5e3cd2601840ed824642750
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2015-03-16 16:52:30 +0000

    Added setAlt to move uav_icon to established altitude

--- app/src/main/java/pt/lsts/asa/fragments/AltitudeBarFragment.java
+++ app/src/main/java/pt/lsts/asa/fragments/AltitudeBarFragment.java
@@ -1,14 +1,17 @@
 package pt.lsts.asa.fragments;
 
 
-import pt.lsts.asa.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import pt.lsts.asa.R;
 
 
 /**
@@ -18,6 +21,7 @@ public class AltitudeBarFragment extends Fragment {
 
     public static final String TAG = "AltitudeBarFragment";
     private FragmentActivity fragmentActivity=null;
+    private ImageView altitudeBarDroneIconimageView = null;
 
     public AltitudeBarFragment() {
         // Required empty public constructor
@@ -31,12 +35,23 @@ public class AltitudeBarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_altitude_bar, container, false);
+        altitudeBarDroneIconimageView = (ImageView) v.findViewById(R.id.altitudeBarDroneIconimageView);
         return v;
     }
 
     @Override
     public void onResume(){
         super.onResume();
+        setAlt(700);//testing
+    }
+
+    /**
+     *
+     * @param alt may be bettween 0 and 1000, available range.
+     */
+    public void setAlt(int alt){
+        FrameLayout.LayoutParams frameLayoutParams = (FrameLayout.LayoutParams) altitudeBarDroneIconimageView.getLayoutParams();
+        frameLayoutParams.setMargins(frameLayoutParams.leftMargin,frameLayoutParams.topMargin,frameLayoutParams.rightMargin,(80-(35)) + alt);//80 bottom margin; 35 is the adjustment value for icon positioning
     }
 
 
